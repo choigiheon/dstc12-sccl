2e4c26f8e873603426fe044c4c41d10ab4251e50
Choi Giheon <anthon0919@naver.com>
2025-04-02 06:01:04 +0900

    Modify parameter in main.py

--- sccl/main.py
+++ sccl/main.py
@@ -49,22 +49,22 @@ def get_args(argv):
     parser.add_argument('--seed', type=int, default=0, help="")
     parser.add_argument('--print_freq', type=float, default=5, help="")
     parser.add_argument('--device', type=str, default='mps', help="")  
-    parser.add_argument('--model_name', type=str, default='sentence-transformers/all-MiniLM-L6-v2', help="")
+    parser.add_argument('--model_name', type=str, default='sentence-transformers/all-mpnet-base-v2', help="")
     parser.add_argument('--dropout', type=float, default=0.1, help="")
     # Dataset
     parser.add_argument('--dataset_file', type=str, default='./dstc12-data/AppenBanking/all.jsonl')
     parser.add_argument('--result_file', type=str, default='./appen_banking_predicted.jsonl', help="결과를 저장할 파일 경로")
     parser.add_argument('--dataname', type=str, default='searchsnippets', help="")
-    parser.add_argument('--num_clusters', type=int, default=10, help="")
+    parser.add_argument('--num_clusters', type=int, default=14, help="")
     parser.add_argument('--max_length', type=int, default=100)
     # Learning parameters
     parser.add_argument('--lr', type=float, default=1e-6, help="")
-    parser.add_argument('--lr_scale', type=int, default=100, help="head에는 lr_scale 적용")
-    parser.add_argument('--max_iter', type=int, default=0)
+    parser.add_argument('--lr_scale', type=int, default=10, help="head에는 lr_scale 적용")
+    parser.add_argument('--max_iter', type=int, default=1000)
     # contrastive learning
     parser.add_argument('--objective', type=str, default='SCCL', choices=['SCCL', 'contrastive'])
     parser.add_argument('--augtype', type=str, default='virtual', choices=['virtual', 'explicit']) # 건들지 말 것.
-    parser.add_argument('--batch_size', type=int, default=200)
+    parser.add_argument('--batch_size', type=int, default=400)
     parser.add_argument('--temperature', type=float, default=0.5, help="temperature required by contrastive loss")
     parser.add_argument('--eta', type=float, default=10, help="")
     
